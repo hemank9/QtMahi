e01451425ce8bd4a430440f6845102dcccf23c9c
Swapnil <swapnilshah5889@gmail.com>
2021-03-18 15:13:59 +0530

    Added constants.py

---
+++ venv/constants.py
@@ -0,0 +1,24 @@
+AppType = "user_app"
+Stuff = "stuff"
+RoleID = "4"
+RegID = "XXXXX"
+
+# API Actions
+Action_PROFILE_REQUIRED_SETUP_CHECK = "PROFILE_REQUIRED_SETUP_CHECK"
+ACTION_GET_MY_APPOINTMENT_LIST = "GET_MY_APPOINTMENT_LIST"
+ACTION_DEVICE_SIGN_IN = "device_sign_in"
+ACTION_my_health_calendar = "my_health_calendar"
+
+# Database Connection Settings
+my_database_name = "mahi.db"
+login_table = "LOGIN_TABLE"
+profile_table = "MY_PROFILE"
+health_calendar_table = "CALENDAR"
+appointments_table = "APPOINTMENTS"
+
+device_id = "mahi09"
+device_token = "mahi09"
+
+#One must always be commented
+# path = r"C:\Users\Dell\PycharmProjects\\" #Hemank Path to project
+path = r"D:\\Projects\\Fluvina\\MAHI\\Mahi Source Code\\" #Swapnil Path to project
