18a48e935fd5ed03fe217c1552038b770b1f11dd
hemank9 <hemank9697@gmail.com>
2021-03-24 10:56:46 +0530

    dynamically adding doc appointments

--- experiment.py
+++ experiment.py
@@ -13,19 +13,22 @@ class QCustomQWidget(QWidget):
 
         super(QCustomQWidget, self).__init__(parent)
 
+
+        style = "background-color: #eee; border-radius : 15; margin-top : 10; margin-bottom : 30; margin-left : 10; margin-right : 10; "
         frame1 = QFrame(self)
         # frame1.setGraphicsEffect(shadow)
-        frame1.setFixedWidth(1190)
-        frame1.setFixedHeight(95)
-        frame1.setContentsMargins(10, 10, 10, 10)
-        frame1.setStyleSheet(" background-color: #eee; border-radius : 15")
+        frame1.setFixedWidth(1210)
+        frame1.setFixedHeight(120)
+        # frame1.setContentsMargins(10, 10, 10, 10)
+        frame1.setStyleSheet(style)
+
 
         frame2 = QFrame(self)
         # frame1.setGraphicsEffect(shadow)
         frame2.setFixedWidth(1210)
-        frame2.setFixedHeight(95)
-        frame2.setContentsMargins(10, 10, 10, 10)
-        frame2.setStyleSheet(" background-color: #eee; border-radius : 15")
+        frame2.setFixedHeight(120)
+        # frame2.setContentsMargins(10, 10, 10, 10)
+        frame2.setStyleSheet(style)
         # frame1.setFrameStyle(QFrame.Raised)
         # frame1.setGeometry(567, 459, 200, 200)
         # frame1.setFrameStyle(QFrame.Panel |
@@ -51,10 +54,11 @@ class QCustomQWidget(QWidget):
         self.textDownQLabel.setStyleSheet("color:" + Colors.Grey8)
 
         self.lblAddress = QLabel()
-        self.lblAddress.setStyleSheet("color:" + Colors.Grey8)
+        self.lblAddress.setStyleSheet("color:" + Colors.Grey8 +";overflow: hidden; text-overflow: ellipsis; display: -webkit-box; "
+                                                               "-webkit-line-clamp: 2; -webkit-box-orient: vertical;")
         # self.lblAddress.setGeometry(589, 142, 276, 69)
         self.lblAddress.setWordWrap(True)
-        self.lblAddress.setMinimumHeight(99)
+        # self.lblAddress.setMinimumHeight(99)
         self.lblAddress.setMinimumWidth(376)
         self.allQHBoxLayout.addWidget(self.lblAddress, 0)
         # self.allQHBoxLayout.setGeometry(QRect(589, 142, 276, 69))
@@ -69,17 +73,18 @@ class QCustomQWidget(QWidget):
         self.allQHBoxLayout.addWidget(self.btnFavorite, 1)
         self.allQHBoxLayout.setContentsMargins(5, 10, 30, 10)
 
+
         shadow1 = QGraphicsDropShadowEffect()
         shadow1.setBlurRadius(15)
-        shadow1.setColor(Qt.green)
+        shadow1.setColor(Qt.lightGray)
         shadow2 = QGraphicsDropShadowEffect()
         shadow2.setBlurRadius(15)
-        shadow2.setColor(Qt.red)
+        shadow2.setColor(Qt.white)
         frame1.setGraphicsEffect(shadow1)
         frame2.setGraphicsEffect(shadow2)
-        # shadow2.setXOffset(5)
-        # shadow2.setYOffset(5)
-
+        shadow2.setXOffset(-5)
+        shadow2.setYOffset(-5)
+        self.allQHBoxLayout.setParent(frame2)
 
 
 
@@ -102,15 +107,47 @@ class exampleQMainWindow(QMainWindow):
         # Create QListWidget
         self.setGeometry(0, 0, 1220, 700)
 
+        self.label2 = QLabel(self)
+        self.label2.setPixmap(QPixmap('Resources\yellow.png'))
+        self.label2.setGeometry(0, 0, 1220, 39)
+
+        self.show()
+        self.setStyleSheet("background-color: #F0F0F3")
+
+
         self.myQListWidget = QListWidget(self)
+        self.myQListWidget.setGeometry(0, 41, 600, 1200)
 
         for index, name, Address in [
             ('Dr. Dhaval Naik', 'Cardiovascular, Thoraic Surgeon',
              'CIMS Hospital,Near Shukan Mall,Opp. Science CityRoad, Shukan Mall,'
-             'Science City Road, Ahmedabad,CIMS Hospital,Near Shukan Mall,Opp. Science CityRoad, Shukan Mall,'
              'Science City Road'),
             ('No.2', 'Nyaruko', 'icon.png'),
             ('No.3', 'Louise', 'icon.png')]:
+            # Create QCustomQWidget
+            myQCustomQWidget = QCustomQWidget()
+            myQCustomQWidget.setTextUp(index)
+            myQCustomQWidget.setTextDown(name)
+            myQCustomQWidget.setAddress(Address)
+            # self.lblm.setGeometry(589, 142, 276, 69)
+            # Create QListWidgetItem
+            myQListWidgetItem = QListWidgetItem(self.myQListWidget)
+            # Set size hint
+            myQListWidgetItem.setSizeHint(myQCustomQWidget.sizeHint())
+            # Add QListWidgetItem into QListWidget
+            self.myQListWidget.addItem(myQListWidgetItem)
+            self.myQListWidget.setItemWidget(myQListWidgetItem, myQCustomQWidget)
+            self.myQListWidget.clear()
+        for index, name, Address in [
+            ('Dr. Swappy', 'Cardiscular, Thoraic Surgeon',
+             'CIMS Hospital,Near Shukan Mall,Opp. Science CityRoad, Shukan Mall,'
+             'Science City Road'),
+            ('No.2', 'Sansuko', 'icon.png'),
+            ('No.2', 'Sansuko', 'icon.png'),
+            ('No.2', 'Sansuko', 'icon.png'),
+            ('No.2', 'Sansuko', 'icon.png'),
+            ('No.2', 'Sansuko', 'icon.png'),
+            ('No.3', 'Ljosdyse', 'icon.png')]:
             # Create QCustomQWidget
             myQCustomQWidget = QCustomQWidget()
             myQCustomQWidget.setTextUp(index)
