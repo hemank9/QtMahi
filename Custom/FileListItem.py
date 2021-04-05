7a58a1571079a6ad94650f80d65641bd7b441304
Swapnil <swapnilshah5889@gmail.com>
2021-04-05 18:26:06 +0530

    File list screen dynamic listing added

---
+++ Custom/FileListItem.py
@@ -0,0 +1,73 @@
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QCheckBox
+from PyQt5.QtCore import *
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
+import colors as Colors
+
+class FileListItem(QWidget):
+    def __init__(self, parent=None):
+
+        super(FileListItem, self).__init__(parent)
+
+
+        style = "background-color: #F0F0F3; border-radius : 15; margin-top : 20; margin-bottom : 20; margin-left : 10; margin-right : 10; "
+
+        fontObj = QFont('Arial',10)
+        frame1 = QFrame(self)
+        # frame1.setGraphicsEffect(shadow)
+        frame1.setFixedWidth(1151)
+        frame1.setFixedHeight(120)
+        # frame1.setContentsMargins(10, 10, 10, 10)
+        frame1.setStyleSheet(style)
+
+
+        self.allQHBoxLayout = QHBoxLayout()
+        frame2 = QFrame(self)
+        # frame1.setGraphicsEffect(shadow)
+        frame2.setFixedWidth(1151)
+        frame2.setFixedHeight(120)
+        frame2.setStyleSheet(style)
+        self.lblFileName = QLabel()
+        self.lblFileName.setFixedHeight(62)
+        self.lblFileName.setStyleSheet("margin-left:20px;color:#006CB5; font:bold; font-size:25px;")
+        self.lblDateDoctor = QLabel()
+        self.lblDateDoctor.setWordWrap(True)
+        self.lblDateDoctor.setStyleSheet("color:#006CB5; font-size:18px; margin-right:20;")
+
+        self.lblExtension = QLabel()
+        self.lblExtension.setWordWrap(True)
+        self.lblExtension.setAlignment(Qt.AlignRight)
+        self.lblExtension.setFixedWidth(200)
+        self.lblExtension.setStyleSheet("color:#006CB5; font-size:18px; margin-right:20;")
+        self.vBox = QVBoxLayout()
+        self.vBox.addWidget(self.lblExtension)
+        self.vBox.setAlignment(Qt.AlignVCenter)
+
+        self.allQHBoxLayout.addWidget(self.lblFileName, 1)
+        self.allQHBoxLayout.addWidget(self.lblDateDoctor, 1)
+        self.allQHBoxLayout.addLayout(self.vBox)
+        self.setLayout(self.allQHBoxLayout)
+        shadow1 = QGraphicsDropShadowEffect()
+        shadow1.setBlurRadius(15)
+        shadow1.setColor(Qt.lightGray)
+        shadow2 = QGraphicsDropShadowEffect()
+        shadow2.setBlurRadius(15)
+        shadow2.setColor(Qt.white)
+        frame1.setGraphicsEffect(shadow1)
+        frame2.setGraphicsEffect(shadow2)
+        shadow2.setXOffset(-5)
+        shadow2.setYOffset(-5)
+        self.allQHBoxLayout.setParent(frame2)
+
+    def setTextFileName(self, text):
+        self.lblFileName.setText(text)
+
+    def setTextDateDoctor(self, text):
+        self.lblDateDoctor.setText(text)
+
+    def setExtension(self, text):
+        self.lblExtension.setText(text)
--- UI/MyFileList.py
+++ UI/MyFileList.py
@@ -3,7 +3,7 @@ from PyQt5 import QtCore, QtGui
 from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 import sys
-from UI.fileView import FileView
+import Custom.FileListItem as myFile
 
 class FileList(QMainWindow):
     def __init__(self):
@@ -21,29 +21,27 @@ class FileList(QMainWindow):
         # self.show()
     #
     def UiComponents(self):
+        self.myQListWidget = QListWidget(self)
+        self.myQListWidget.setGeometry(16, 125, 1191, 475)
 
+        for i in range(5):
+            myQCustomQWidget = myFile.FileListItem()
+            myQCustomQWidget.setTextFileName('X-Ray')
+            myQCustomQWidget.setTextDateDoctor('24/12/2021 Dr.Mobihealth')
+            myQCustomQWidget.setExtension(".jpeg")
+            myQListWidgetItem = QListWidgetItem(self.myQListWidget)
+            myQListWidgetItem.setSizeHint(myQCustomQWidget.sizeHint())
 
-        btn_file1 = QPushButton("", self)
-        btn_file1.setGeometry(41, 150, 1138, 76)
-        btn_file1.setStyleSheet("border-radius : 10; color:#FFFFFF; text-align:left")
-        btn_file1.setIcon(QtGui.QIcon('..\Resources\mfile1.png'))
-        btn_file1.setIconSize(QtCore.QSize(1138, 76))
-        btn_file1.clicked.connect(self.file)
+            self.myQListWidget.addItem(myQListWidgetItem)
+            self.myQListWidget.setItemWidget(myQListWidgetItem, myQCustomQWidget)
 
-        btn_file2 = QPushButton("", self)
-        btn_file2.setGeometry(41, 236, 1138, 76)
-        btn_file2.setStyleSheet("border-radius : 10; background-color : #7ACEDA; color:#FFFFFF; text-align:left")
-        btn_file2.setIcon(QtGui.QIcon('..\Resources\myfile2.png'))
-        btn_file2.setIconSize(QtCore.QSize(1138, 76))
-        # btn_file2.clicked.connect(self.file)
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
 
-        btn_file3 = QPushButton("", self)
-        btn_file3.setGeometry(41, 327, 1138, 76)
-        btn_file3.setStyleSheet("border-radius : 10; background-color : #7ACEDA; color:#FFFFFF; text-align:left")
-        btn_file3.setIcon(QtGui.QIcon('..\Resources\myfile3.png'))
-        btn_file3.setIconSize(QtCore.QSize(1138, 76))
-        # btn_file3.clicked.connect(self.file)
+    # create the instance of our Window
+    window = FileList()
 
-    def file(self):
-            self.m = FileView()
-            self.m.show()
+    window.show()
+
+    # start the app
+    sys.exit(App.exec_())
