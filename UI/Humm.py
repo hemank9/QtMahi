995c0951570572f47a2aa84882e4e861afa1f79c
hemank9 <hemank9697@gmail.com>
2021-04-19 17:09:18 +0530

    Added Buttons in HUMM

--- UI/Humm.py
+++ UI/Humm.py
@@ -22,6 +22,70 @@ class Humm(QWidget):
 
     def UiComponents(self):
 
+        # BUTTONS FOR HUMM
+
+        btnBackToTop = QPushButton(self)
+        btnBackToTop.setGeometry(1136, 254, 61, 61)
+        btnBackToTop.setStyleSheet("border-radius:10")
+        btnBackToTop.setGraphicsEffect(Util.getNeuShadow(0))
+
+        btnBackToTop1 = QPushButton(self)
+        btnBackToTop1.setGeometry(1136, 254, 61, 61)
+        btnBackToTop1.setGraphicsEffect(Util.getNeuShadow(1))
+        btnBackToTop1.setStyleSheet("border-radius:10")
+        btnBackToTop1.setIcon(QtGui.QIcon('../Resources/backToTop.png'))
+        btnBackToTop1.setIconSize(QtCore.QSize(60, 40))
+
+        btnRefresh = QPushButton(self)
+        btnRefresh.setGeometry(1136, 340, 61, 61)
+        btnRefresh.setStyleSheet("border-radius:10")
+        btnRefresh.setGraphicsEffect(Util.getNeuShadow(0))
+
+        btnRefresh1 = QPushButton(self)
+        btnRefresh1.setGeometry(1136, 340, 61, 61)
+        btnRefresh1.setGraphicsEffect(Util.getNeuShadow(1))
+        btnRefresh1.setStyleSheet("border-radius:10")
+        btnRefresh1.setIcon(QtGui.QIcon('../Resources/Refresh.png'))
+        btnRefresh1.setIconSize(QtCore.QSize(60, 40))
+
+        btnHome = QPushButton(self)
+        btnHome.setGeometry(1136, 426, 61, 61)
+        btnHome.setStyleSheet("border-radius:10")
+        btnHome.setGraphicsEffect(Util.getNeuShadow(0))
+
+        btnHome1 = QPushButton(self)
+        btnHome1.setGeometry(1136, 426, 61, 61)
+        btnHome1.setGraphicsEffect(Util.getNeuShadow(1))
+        btnHome1.setStyleSheet("border-radius:10")
+        btnHome1.setIcon(QtGui.QIcon('../Resources/Home.png'))
+        btnHome1.setIconSize(QtCore.QSize(60, 40))
+
+        btnPrevious = QPushButton(self)
+        btnPrevious.setGeometry(1136, 512, 61, 61)
+        btnPrevious.setStyleSheet("border-radius:10")
+        btnPrevious.setGraphicsEffect(Util.getNeuShadow(0))
+
+        btnPrevious1 = QPushButton(self)
+        btnPrevious1.setGeometry(1136, 512, 61, 61)
+        btnPrevious1.setGraphicsEffect(Util.getNeuShadow(1))
+        btnPrevious1.setStyleSheet("border-radius:10")
+        btnPrevious1.setIcon(QtGui.QIcon('../Resources/Previous.png'))
+        btnPrevious1.setIconSize(QtCore.QSize(60, 40))
+
+        btnNext = QPushButton(self)
+        btnNext.setGeometry(1136, 598, 61, 61)
+        btnNext.setStyleSheet("border-radius:10")
+        btnNext.setGraphicsEffect(Util.getNeuShadow(0))
+
+        btnNext1 = QPushButton(self)
+        btnNext1.setGeometry(1136, 598, 61, 61)
+        btnNext1.setGraphicsEffect(Util.getNeuShadow(1))
+        btnNext1.setStyleSheet("border-radius:10")
+        btnNext1.setIcon(QtGui.QIcon('../Resources/Next.png'))
+        btnNext1.setIconSize(QtCore.QSize(60, 40))
+
+
+
         url_image= "https://mobihealth.in//upload/health_feed/607b1f0aa40591618681610672.png"
         image = QImage()
         image.loadFromData(requests.get(url_image).content)
